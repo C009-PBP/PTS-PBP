c9f4846518f60d16a98e4ab2de15ec9a14a5537f
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-10-28 18:09:36 +0700

    add pasien & dokter authentication v2

--- tanya_dokter/models.py
+++ tanya_dokter/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-
+from authentication.models import User
 # Create your models here.
 class Forum(models.Model):
-    question_id = models.ForeignKey(on_delete=models.CASCADE)
+    question_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
     date = models.DateField(null=False, blank=False, auto_now_add=True)
     specialization = models.CharField(max_length=100)
     title = models.CharField(max_length=255)
