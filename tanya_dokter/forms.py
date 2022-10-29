260d09332c7f651627fd6481ec73b4a7eb5e8c1c
triviaspace <shafatriviaa@gmail.com>
2022-10-29 09:40:15 +0700

    bismillah

--- tanya_dokter/forms.py
+++ tanya_dokter/forms.py
@@ -6,4 +6,4 @@ from .models import Forum
 class ForumForm(forms.ModelForm):
     class Meta:
         model = Forum
-        fields = ('specialization', 'title', 'question_text')
+        fields = ('specialization', 'title', 'question')
--- tanya_dokter/urls.py
+++ tanya_dokter/urls.py
@@ -5,7 +5,11 @@ app_name = 'tanya_dokter'
 
 urlpatterns = [
     # path('forum/fill', views.create_forum, name='forum'),
-    path('view/', views.show_landing_page, name='landing_page'),
+    path('', views.show_landing_page, name='landing_page'),
     path('show/forum/', views.show_forum, name='show_forum'),
     path('add_question/', views.add_question_ajax, name='add_question_ajax'),
+    path('json/', views.form_ajax, name='form_ajax'),
+    # path('view/', views.ForumView.as_view(), name='forum_view'),
+    # path('create/forum', views.CreateForum.as_view(), name='create_forum'),
+    
 ]
