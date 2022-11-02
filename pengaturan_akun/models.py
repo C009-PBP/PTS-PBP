e9a5f262aaadc67d909e850a0b524224531b4b15
Najmi Anasya Calyla <najmi.anasya@ui.ac.id>
2022-11-02 18:18:07 +0700

    add medical record page

--- pengaturan_akun/models.py
+++ pengaturan_akun/models.py
@@ -5,8 +5,7 @@ from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE, db_constraint=False)
-    # user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
@@ -17,6 +16,19 @@ class Profile(models.Model):
     street = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=255, blank=True, null=True)
     province = models.CharField(max_length=255, blank=True, null=True)
+
+    emergency_firstname = models.CharField(max_length=255, blank=True, null=True)
+    emergency_lastname = models.CharField(max_length=255, blank=True, null=True)
+    emergency_relationship = models.CharField(max_length=255, blank=True, null=True)
+    emergency_phone_no = models.CharField(max_length=20, blank=True, null=True)
+    emergency_street = models.CharField(max_length=255, blank=True, null=True)
+    emergency_city = models.CharField(max_length=255, blank=True, null=True)
+    emergency_province = models.CharField(max_length=255, blank=True, null=True)
+
+    bloodtype = models.CharField(max_length=255, blank=True, null=True)
+    bloodpressure = models.CharField(max_length=255, blank=True, null=True)
+    illnesses = models.CharField(max_length=255, blank=True, null=True)
+    allergies = models.CharField(max_length=255, blank=True, null=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
