5920bb1dfc2be8097401de5d5248db41a87fab7e
Najmi Anasya Calyla <najmi.anasya@ui.ac.id>
2022-11-02 10:21:51 +0700

    Add Pengaturan Akun

--- pengaturan_akun/models.py
+++ pengaturan_akun/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from authentication.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE, db_constraint=False)
+    # user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    phone_no = models.CharField(max_length=20, blank=True, null=True)
+    profile_pic = models.ImageField(blank=True, null=True, upload_to='images/profile')
+    birth_date = models.DateField(max_length=8, blank=True, null=True)
+    gender = models.CharField(max_length=20, blank=True, null=True)
+    street = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=255, blank=True, null=True)
+    province = models.CharField(max_length=255, blank=True, null=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
