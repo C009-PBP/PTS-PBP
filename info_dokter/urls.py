f2f3e4e34425ecdeb802f4645722d8599ba539ee
rezataufiq56 <rezataufiq56@gmail.com>
2022-12-10 11:35:44 +0700

    Update views dan URLS

--- info_dokter/urls.py
+++ info_dokter/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('add/', add_review, name='add_review'),
     path('json/', show_json, name='show_json'),
     path ('json2/', show_json2, name='show_json2'),
+    path('json-flutter/<int:userId>', show_json_flutter, name='show_json_flutter'),
 ]
