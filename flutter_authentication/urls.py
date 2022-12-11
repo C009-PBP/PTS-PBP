8de97d13b8d67566f1b8e2f558b0c5d3788af8e6
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-12-11 21:07:06 +0700

    update register logic for flutter

--- flutter_authentication/urls.py
+++ flutter_authentication/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', login, name='login'), #?
     path('logout/', logout, name='logout'),
     path('user-data/', get_user_data, name='get_user_data'), 
+    path('register_pasien/', register_pasien, name='register_pasien'),
+    path('register_dokter/', register_dokter, name='register_dokter'),
 ]
