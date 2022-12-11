cdde05b4b20c245a607a8c6597936843bd5505b1
rezaty <87021366+rezaty@users.noreply.github.com>
2022-12-11 11:47:29 +0700

    Update views.py

--- info_dokter/views.py
+++ info_dokter/views.py
@@ -37,9 +37,8 @@ def show_json2(request):
     orang = request.user
     dataReview2 = ReviewDokter.objects.filter(user = orang)
     return HttpResponse(serializers.serialize("json", dataReview2), content_type="application/json")
-def show_json_flutter(request, userId):
-    userId = request.user
-    infoDokter_objects = ReviewDokter.objects.filter(user=userId)
-    return HttpResponse(serializers.serialize("json", dataReview3), content_type="application/json")
 
+def show_json_flutter(request, userId):
+    dataReviewFlutter = ReviewDokter.objects.filter(user=userId)
+    return HttpResponse(serializers.serialize("json", dataReviewFlutter), content_type="application/json")
 
